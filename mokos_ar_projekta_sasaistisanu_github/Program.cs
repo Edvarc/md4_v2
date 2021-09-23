bdf60597bab0de7af6829bdba9a011a3a7a0a8fe
Edvarts Krakts <edvarts.krakts@gmail.com>
2021-09-23 15:13:46 +0300

    teststet

---
+++ mokos_ar_projekta_sasaistisanu_github/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mokos_ar_projekta_sasaistisanu_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
